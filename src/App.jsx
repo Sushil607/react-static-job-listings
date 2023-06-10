872488fe20f364620fa3bdb02e50f8d11e497cd7
Sushil607 <bodadesushil@gmail.com>
2023-06-10 20:06:32 +0530

    added FilterTag styles

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,13 @@ import JobListingsData from "./data/data.json"
 
 // Importing Components
 import Header from "./components/Header"
+import FilterTagList from "./components/FilterTagList"
 import JobListing from "./components/JobListing"
 
 function App() {
   const [isLoading,setIsLoading] = useState(false)
   const [jobListings,setJobListings] = useState([])
+  const [filterTagsList,setFilterTagsList] = useState(["Frontend","CSS"])
   
   useEffect(()=> {
     setIsLoading(true)
@@ -23,6 +25,7 @@ function App() {
     <>  
       <Header/>
       <main>
+        { filterTagsList.length>0 && <FilterTagList filterTagsList={filterTagsList}/>}
         <div className="job-listings-container">
           {isLoading ? <div>Loading...</div> : (
             jobListings.map((jobListing) => <JobListing key={jobListing.id} jobListing={jobListing}/>)
