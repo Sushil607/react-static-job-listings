b835badc8dc075ac0500d100bb2776155179a750
Sushil607 <bodadesushil@gmail.com>
2023-06-10 18:43:22 +0530

    created JobListing and Tag components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,33 @@
+import {useState,useEffect} from "react"
+
+// Importing Data
+import JobListingsData from "./data/data.json"
+
+// Importing Components
+import JobListing from "./components/JobListing"
+
 function App() {
+  const [isLoading,setIsLoading] = useState(false)
+  const [jobListings,setJobListings] = useState([])
+  
+  useEffect(()=> {
+    setIsLoading(true)
+    setTimeout(()=> {
+      setJobListings(JobListingsData)
+      setIsLoading(false)
+    },1000)
+  },[])
+
   return (
-    <>test</>
+    <>
+      <main>
+        <div className="job-listings-container">
+          {isLoading ? <div>Loading...</div> : (
+            jobListings.map((jobListing) => <JobListing key={jobListing.id} jobListing={jobListing}/>)
+          )}
+        </div>
+      </main>
+    </>
   )
 }
 
